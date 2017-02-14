9728373df629ee521dc74dee0cfd741423341217
ellisderham1 <ellisderham@gmail.com>
2017-02-14 16:23:13 +0000

    Tidy Up

--- app/index.php
+++ app/index.php
@@ -1,49 +1,3 @@
-<?php
-
-session_start();
-
-$servername = "mysql.hostinger.co.uk";
-$dbusername = "u495998595_admin";
-$dbpassword = "dmftw38QkZNB";
-$dbname = "u495998595_admin";
-
-$LoginUsername = trim(strip_tags(addslashes($_REQUEST['LoginUsername'])));
-$LoginPassword = $_REQUEST['LoginPassword'];
-
-$con = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
-
-$loginquery  = "SELECT * FROM User WHERE Username = '" . $LoginUsername . "';";
-
-$result = mysqli_query($con, $loginquery);
-
-if (mysqli_num_rows($result) > 0) {
-
-  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-  $facultyquery  = "SELECT * FROM Faculty WHERE FacultyID = '" . $row['FacultyID'] . "';";
-  $rolequery  = "SELECT * FROM Role WHERE RoleID = '" . $row['RoleID'] . "';";
-  $result2 = mysqli_query($con, $facultyquery);
-  $result3 = mysqli_query($con, $rolequery);
-  $row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
-  $row3 = mysqli_fetch_array($result3, MYSQLI_ASSOC);
-
-  //Logged In Variables
-  $_SESSION["user_logged_in"] = true;
-  $_SESSION["failed_login"] = false;
-  $_SESSION["Username"] = $row['Username'];
-  $_SESSION["Faculty"] = $row2['FacultyName'];;
-  $_SESSION["Role"] = $row3['RoleName'];
-
-} else {
-    if ($LoginUsername != ''|| $LoginPassword != ''){
-    	//Error Variables
-    	$_SESSION["failed_login"] = true;
-    }
-}
-
-mysqli_close($con);
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -75,9 +29,7 @@ mysqli_close($con);
 			<li class="w3-hide-small w3-right">
 				<a class="w3-hover-red" id="loginButton" onclick="document.getElementById('modal-logged-out').style.display='block'"><i class="fa fa-user"></i> <span class="loginClass" id="loginButtonID">Sign In</span></a>
 			</li>
-
-		</ul>
-		<!-- Navbar on small screens -->
+		</ul><!-- Navbar on small screens -->
 		<div class="w3-hide w3-hide-large w3-hide-medium" id="navDemo">
 			<ul class="w3-navbar w3-left-align w3-white">
 				<li>
@@ -97,16 +49,12 @@ mysqli_close($con);
 				</li>
 			</ul>
 		</div>
-	</div>
-
-	<!-- First Parallax Image with Logo Text -->
+	</div><!-- First Parallax Image with Logo Text -->
 	<div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
 		<div class="w3-display-middle w3-display-middle2" style="white-space:nowrap;">
 			<span class="w3-center w3-padding-xlarge w3-black w3-xlarge w3-wide w3-animate-opacity homepage-text">GREENMAG</span>
 		</div>
-	</div>
-
-	<!-- Container (About Section) -->
+	</div><!-- Container (About Section) -->
 	<div class="w3-content w3-container w3-padding-32" id="about">
 		<h3 class="w3-center">ABOUT</h3>
 		<p class="w3-center"><em>Lorem Ipsum</em></p>
@@ -130,16 +78,12 @@ mysqli_close($con);
 			<span class="w3-xlarge">1</span><br>
 			Greenwich
 		</div>
-	</div>
-
-	<!-- Second Parallax Image with Portfolio Text -->
+	</div><!-- Second Parallax Image with Portfolio Text -->
 	<div class="bgimg-2 w3-display-container w3-opacity-min">
 		<div class="w3-display-middle">
 			<span class="w3-xxlarge w3-text-light-grey w3-wide w3-text-shadow-parallax">ARTICLES</span>
 		</div>
-	</div>
-
-	<!-- Container (Portfolio Section) -->
+	</div><!-- Container (Portfolio Section) -->
 	<div class="w3-content w3-container w3-padding-64" id="articles">
 		<h3 class="w3-center">SUBMITTED WORK</h3>
 		<p class="w3-center"><em>Here are some examples of work submitted by our students.</em></p><br>
@@ -156,25 +100,19 @@ mysqli_close($con);
 			<div class="w3-col m3"><img alt="Empty ghost train" class="w3-hover-opacity" onclick="onClick(this)" src="../images/logo_dark.png" style="width:100%"></div>
 			<div class="w3-col m3"><img alt="Sailing" class="w3-hover-opacity" onclick="onClick(this)" src="../images/logo_dark.png" style="width:100%"></div><button class="w3-btn w3-light-grey w3-padding-xlarge" style="margin-top:64px">LOAD MORE</button>
 		</div>
-	</div>
-
-	<!-- Modal for full size images on click-->
+	</div><!-- Modal for full size images on click-->
 	<div class="w3-modal w3-black" id="modal01" onclick="this.style.display='none'">
 		<span class="w3-closebtn w3-text-white w3-opacity w3-hover-opacity-off w3-xxlarge w3-container w3-display-topright" title="Close Modal Image"><i class="fa fa-remove"></i></span>
 		<div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
 			<img class="w3-image" id="img01">
 			<p class="w3-opacity w3-large" id="caption"></p>
 		</div>
-	</div>
-
-	<!-- Third Parallax Image with Portfolio Text -->
+	</div><!-- Third Parallax Image with Portfolio Text -->
 	<div class="bgimg-3 w3-display-container w3-opacity-min">
 		<div class="w3-display-middle">
 			<span class="w3-xxlarge w3-text-light-grey w3-wide w3-text-shadow-parallax">STATISTICS</span>
 		</div>
-	</div>
-
-	<!-- Container (Contact Section) -->
+	</div><!-- Container (Contact Section) -->
 	<div class="w3-content w3-container w3-padding-64" id="statistics">
 		<h3 class="w3-center">STATS:</h3>
 		<p class="w3-large w3-center w3-padding-16">Some statistics about our submissions:</p>
@@ -191,8 +129,6 @@ mysqli_close($con);
 			<div class="w3-progressbar" style="width:20%"></div>
 		</div>
 	</div>
-	
-
 	<div class="w3-modal" id="modal-logged-out">
 		<div class="w3-modal-content w3-animate-top w3-card-8">
 			<header class="w3-container w3-blackgrey">
@@ -203,8 +139,7 @@ mysqli_close($con);
 				<div class="w3-half w3-padding-right-small">
 					<form action="form.asp" target="_blank">
 						<div class="w3-row-padding-top">
-						<h3>New Account</h3>
-							<input class="w3-input w3-border w3-register-input" name="Username" placeholder="Email Address" required="" type="text"> <input class="w3-input w3-border w3-register-input" minlength="5" maxlength="20" name="Password" placeholder="Password" required="" type="password"> <input class="w3-input w3-border w3-register-input" minlength="5" maxlength="20" name="ConfirmPassword" placeholder="Confirm Password" required="" type="password"> <select class="styled-select w3-select w3-border w3-register-input" name="option">
+							<h3>New Account</h3><input class="w3-input w3-border w3-register-input" name="Username" placeholder="Email Address" required="" type="text"> <input class="w3-input w3-border w3-register-input" maxlength="20" name="Password" placeholder="Password" required="" type="password"> <input class="w3-input w3-border w3-register-input" maxlength="20" name="ConfirmPassword" placeholder="Confirm Password" required="" type="password"> <select class="styled-select w3-select w3-border w3-register-input" name="option">
 								<option disabled selected value="">
 									Choose your faculty
 								</option>
@@ -221,26 +156,20 @@ mysqli_close($con);
 									Faculty of Engineering & Science
 								</option>
 							</select>
-						</div>
-						 <button class="w3-btn w3-padding w3-section" id="register" type="submit"><i class="fa fa-check"></i> Register</button>
+						</div><button class="w3-btn w3-padding w3-section" id="register" type="submit"><i class="fa fa-check"></i> Register</button>
 					</form>
 				</div>
 				<div class="w3-half w3-padding-left-small">
 					<form action="" method="post">
 						<div class="w3-row-padding-top">
-						<h3>Existing Account</h3>
-							<input class="w3-input w3-border w3-register-input" name="LoginUsername" placeholder="Email Address" required="" type="text">
-							<input class="w3-input w3-border w3-register-input" name="LoginPassword" placeholder="Password" minlength="4" maxlength="20" required="" type="password">
-						</div>
-						 <button class="w3-btn w3-padding w3-section" id="register" type="submit"><i class="fa fa-check"></i> Login</button><a id="forgottenpassword" style="padding-left:10px !important;">Forgotten Password?</a>
-						 <h4 id="invalidid" style="display:none;padding-top:0px !important;color:red;font-size: 14px;">Invalid Email Address or Password</h4>
+							<h3>Existing Account</h3><input class="w3-input w3-border w3-register-input" name="LoginUsername" placeholder="Email Address" required="" type="text"> <input class="w3-input w3-border w3-register-input" maxlength="20" name="LoginPassword" placeholder="Password" required="" type="password">
+						</div><button class="w3-btn w3-padding w3-section" id="register" type="submit"><i class="fa fa-check"></i> Login</button><a id="forgottenpassword" style="padding-left:10px !important;">Forgotten Password?</a>
+						<h4 id="invalidid" style="display:none;padding-top:0px !important;color:red;font-size: 14px;">Invalid Email Address or Password</h4>
 					</form>
 				</div>
 			</div>
 		</div>
 	</div>
-
-
 	<div class="w3-modal" id="modal-logged-in">
 		<div class="w3-modal-content w3-animate-top w3-card-8">
 			<header class="w3-container w3-blackgrey">
@@ -248,14 +177,14 @@ mysqli_close($con);
 				<h2><span class="w3-closebtn2 righty" onclick="document.getElementById('modal-logged-in').style.display='none'"><i class="fa fa-close"></i></span></h2>
 			</header>
 			<div class="w3-container">
-				<form action="" method="post" class="w3-center w3-padding-xlarge">
-					<br><div class="w3-center"><img src="../images/1.png" width="150px"></div>
-					<button id="logOut" class="w3-btn w3-padding w3-section" onclick="<?php session_destroy(); ?>">Log Out</button>
+				<form action="logout.php" class="w3-center w3-padding-xlarge" method="post">
+					<br>
+					<div class="w3-center"><img src="../images/1.png" width="150px"></div>
+					<p id="userContent"></p><button class="w3-btn w3-padding w3-section" id="logOut" onclick="">Log Out</button>
 				</form>
 			</div>
 		</div>
 	</div>
-
 	<div class="w3-modal" id="modal-forgotten-password">
 		<div class="w3-modal-content w3-animate-top w3-card-8">
 			<header class="w3-container w3-blackgrey">
@@ -263,74 +192,108 @@ mysqli_close($con);
 				<h2><span class="w3-closebtn2 righty" onclick="document.getElementById('modal-forgotten-password').style.display='none'"><i class="fa fa-close"></i></span></h2>
 			</header>
 			<div class="w3-container">
-				<form action="" method="post" class="w3-center w3-padding-xlarge">
-					<input class="w3-input w3-border w3-register-input" name="ForgotUsername" placeholder="Email Address" required="" type="text">
-					<button id=forgotPassword class="w3-btn w3-padding w3-section" onclick="">Send New Password</button>
+				<form action="" class="w3-center w3-padding-xlarge" method="post">
+					<input class="w3-input w3-border w3-register-input" name="ForgotUsername" placeholder="Email Address" required="" type="text"> <button class="w3-btn w3-padding w3-section" id="forgotPassword" onclick="">Send New Password</button>
 				</form>
 			</div>
 		</div>
-	</div>
-
-	<!-- Footer -->
+	</div><!-- Footer -->
 	<footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
 		<a class="w3-btn w3-padding w3-light-grey w3-hover-grey" href="#home"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
 		<div class="w3-xlarge w3-section">
 			<i class="fa fa-facebook-official w3-hover-text-indigo"></i> <i class="fa fa-instagram w3-hover-text-purple"></i> <i class="fa fa-snapchat w3-hover-text-yellow"></i> <i class="fa fa-pinterest-p w3-hover-text-red"></i> <i class="fa fa-twitter w3-hover-text-light-blue"></i> <i class="fa fa-linkedin w3-hover-text-indigo"></i>
 		</div>
-		<p>Created by Kung Fu Pandas <i class="fa fa-file-code-o"></i> .</p>
+		<p>Created by Kung Fu Pandas</p>
 	</footer>
+	<?php
+
+    session_start();
+
+    $servername = "mysql.hostinger.co.uk";
+    $dbusername = "u495998595_admin";
+    $dbpassword = "dmftw38QkZNB";
+    $dbname = "u495998595_admin";
+
+    $LoginUsername = trim(strip_tags(addslashes($_REQUEST['LoginUsername'])));
+    $LoginPassword = md5($_REQUEST['LoginPassword']);
+
+    $con = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+    $loginquery  = "SELECT * FROM User WHERE Username = '" . $LoginUsername . "';";
+
+    $result = mysqli_query($con, $loginquery);
+
+    if (mysqli_num_rows($result) > 0) {
+
+      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+      $facultyquery  = "SELECT * FROM Faculty WHERE FacultyID = '" . $row['FacultyID'] . "';";
+      $rolequery  = "SELECT * FROM Role WHERE RoleID = '" . $row['RoleID'] . "';";
+      $result2 = mysqli_query($con, $facultyquery);
+      $result3 = mysqli_query($con, $rolequery);
+      $row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+      $row3 = mysqli_fetch_array($result3, MYSQLI_ASSOC);
+
+      //Logged In Variables
+      $_SESSION["user_logged_in"] = true;
+      $_SESSION["failed_login"] = false;
+      $_SESSION["Username"] = $row['Username'];
+      $_SESSION["Faculty"] = $row2['FacultyName'];;
+      $_SESSION["Role"] = $row3['RoleName'];
+
+    } else {
+    	//Failed Login
+    }
+
+    mysqli_close($con);
+
+    ?>
 	<script src="main.js">
 	</script> 
 	<script src="https://code.jquery.com/jquery-3.1.1.js">
 	</script> 
 	<script type="text/javascript">
-	 var Username;
-	 var FacultyName;
-	 var RoleName;
-	 var user_logged_in;
-	 var loggedoutmodal = document.getElementById('modal-logged-out').style.display;
-	 var loggedinmodal = document.getElementById('modal-logged-in').style.display;
+	    var Username;
+	    var FacultyName;
+	    var RoleName;
+	    var user_logged_in;
+	    var loggedoutmodal = document.getElementById('modal-logged-out').style.display;
+	    var loggedinmodal = document.getElementById('modal-logged-in').style.display;
 
-	 function jsLoginFunction(Username, FacultyName, RoleName, user_logged_in){
-	   if (user_logged_in == 1) {
-	     var span = document.getElementById("loginButtonID");
-	     span.textContent = " " + Username;
+	    function jsLoginFunction(Username, FacultyName, RoleName, user_logged_in){
+	      if (user_logged_in == 1) {
+	        var span = document.getElementById("loginButtonID");
+	        span.textContent = " " + Username;
+	        document.getElementById("userContent").innerHTML = "Username: " + Username + "; Faculty Name: " + FacultyName + "; Role Name: " + RoleName; 
 
-	     $("#loginButton").bind("click", function() {
-	       document.getElementById('modal-logged-out').style.display='none';
-	       document.getElementById('modal-logged-in').style.display='block';
-	     });
+	        $("#loginButton").bind("click", function() {
+	          document.getElementById('modal-logged-out').style.display='none';
+	          document.getElementById('modal-logged-in').style.display='block';
+	        });
+	      }
+	    }
+
+	    $(document).keyup(function(e) {
+	    if (e.keyCode == 27) { // escape key maps to keycode `27`
+	       console.log("escape pressed");
+	       if(document.getElementById('modal-logged-in').style.display == 'block'){
+	           document.getElementById('modal-logged-in').style.display = 'none';
+	       }
+	       else if(document.getElementById('modal-logged-out').style.display == 'block'){
+	           document.getElementById('modal-logged-out').style.display = 'none';
+	       }
+	       else if(document.getElementById('modal-forgotten-password').style.display == 'block'){
+	           document.getElementById('modal-forgotten-password').style.display = 'none';
+	       }
 	   }
-	 }
-
-	 function jsFailedLoginFunction(failed_login){
-	 	if(failed_login != ''){
-	 		console.log("Failed Login" + failed_login);
-	 		document.getElementById("invalidid").style.display = "block";
-	 		document.getElementById('modal-logged-out').style.display='block'
-	 	}
-	 }
-
-
-	 $(document).keyup(function(e) {
-     if (e.keyCode == 27) { // escape key maps to keycode `27`
-        console.log("escape pressed");
-        if(document.getElementById('modal-logged-in').style.display == 'block'){
-        	document.getElementById('modal-logged-in').style.display = 'none';
-        }
-        else if(document.getElementById('modal-logged-out').style.display == 'block'){
-        	document.getElementById('modal-logged-out').style.display = 'none';
-        }
-    }
-});
-	 $( "#forgottenpassword" ).click(function() {
-  	 	document.getElementById("modal-forgotten-password").style.display = "block";
-	 	document.getElementById('modal-logged-out').style.display='none';
-});
+	});
+	    $( "#forgottenpassword" ).click(function() {
+	       document.getElementById("modal-forgotten-password").style.display = "block";
+	       document.getElementById('modal-logged-out').style.display='none';
+	});
 
 	</script> <?php
-	echo "<script type='text/javascript'>jsLoginFunction('" . $_SESSION['Username'] . "','" . $_SESSION['Faculty'] . "', '" . $_SESSION['Role'] . "', '" . $_SESSION['user_logged_in'] . "')</script>";
-	echo "<script type='text/javascript'>jsFailedLoginFunction('" . $_SESSION['failed_login'] . "')</script>";
-	?>
+	    echo "<script type='text/javascript'>jsLoginFunction('" . $_SESSION['Username'] . "','" . $_SESSION['Faculty'] . "', '" . $_SESSION['Role'] . "', '" . $_SESSION['user_logged_in'] . "')</script>";
+	    ?>
 </body>
 </html>
